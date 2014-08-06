87dfb3cccbb03c6a73667456c0c8ca0f94e39d85
DanBely <gekugiog@fakeinbox.com>
2014-08-06 20:11:54 +0400

    Revert "for mozila 26"!!!
    This reverts commit 58ca51fb50ee5959f7f201181ea02a1c529599a0.
    
    Conflicts:
    	src/test/java/TestForms/ForumCommentForm.java
    	src/test/java/TestForms/LoginForm.java
    	src/test/java/TestForms/NewsCommentForm.java
    	src/test/java/TestForms/ProfileForm.java
    	src/test/java/Tests/MainTest.java
    	src/test/java/webdriver/elements/Select.java
    	target/aqa-framework-1.2.9-SNAPSHOT-tests.jar
    	target/aqa-framework-1.2.9-SNAPSHOT.jar
    	target/surefire-reports/TEST-TestSuite.xml
    	target/surefire-reports/TestSuite.txt
    	target/test-classes/TestForms/ForumCommentForm.class
    	target/test-classes/TestForms/LoginForm.class
    	target/test-classes/TestForms/NewsCommentForm.class
    	target/test-classes/TestForms/ProfileForm.class
    	target/test-classes/Tests/MainTest.class
    	target/test-classes/webdriver/elements/Select.class

--- src/test/java/Tests/MainTest.java
+++ src/test/java/Tests/MainTest.java
@@ -20,33 +20,33 @@ public class MainTest extends BaseTest {
         
         TestLogin tl = new TestLogin();
         tl.runTest();
-//        
-//        TestSearchAndCompare tsac = new TestSearchAndCompare();
-//        tsac.runTest();
-//        
-//        TestCreateAdvert tca = new TestCreateAdvert();
-//        tca.runTest();
+        
+        TestSearchAndCompare tsac = new TestSearchAndCompare();
+        tsac.runTest();
+        
+        TestCreateAdvert tca = new TestCreateAdvert();
+        tca.runTest();
           
         TestFindMoreItems tftmi = new TestFindMoreItems();
         tftmi.runTest();
           
-//        TestForumComment tfc = new TestForumComment();
-//        tfc.runTest();
-//          
-//        TestItem ti = new TestItem();
-//        ti.runTest();
-//        
-//        TestThemeForum ttf = new TestThemeForum();
-//        ttf.runTest();
-//        
-//        TestSendMessage tsm = new TestSendMessage();
-//        tsm.runTest();
-//        
-//        TestProfile tp = new TestProfile();
-//        tp.runTest();
-//        
-//        TestNewsComment tnc = new TestNewsComment();
-//        tnc.runTest();
+        TestForumComment tfc = new TestForumComment();
+        tfc.runTest();
+          
+        TestItem ti = new TestItem();
+        ti.runTest();
+        
+        TestThemeForum ttf = new TestThemeForum();
+        ttf.runTest();
+        
+        TestSendMessage tsm = new TestSendMessage();
+        tsm.runTest();
+        
+        TestProfile tp = new TestProfile();
+        tp.runTest();
+        
+        TestNewsComment tnc = new TestNewsComment();
+        tnc.runTest();
 
     }
     
